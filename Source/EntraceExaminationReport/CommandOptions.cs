1475e7b3ecad08866c97b48c85608b2b15fd1b01
Tomas Kubes <kubes.tomas@gmail.com>
2018-07-25 22:08:52 +0200

    Command line parsing with error reporting

---
+++ Source/EntraceExaminationReport/CommandOptions.cs
@@ -0,0 +1,22 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntraceExaminationReport
+{
+
+    public class CommandOptions
+    {
+        [Option('i', "input", Required = true, HelpText = "Input file to be processed.")]
+        public string InputFileName { get; set; }
+
+        [Option('f', "format", Required = false, Default = ReportFormat.XML, HelpText = "Output files format (XML/JSON).")]
+        public ReportFormat ReportFormat { get; set; }
+
+        [Option('o', "output", Required = true, HelpText = "Output directory for saving reports.")]
+        public string OutputDirectory { get; set; }        
+    }
+}
--- Source/EntraceExaminationReport/Program.cs
+++ Source/EntraceExaminationReport/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using CommandLine;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,14 +12,26 @@ namespace EntraceExaminationReport
     {
         static void Main(string[] args)
         {
-            string pathInput = "";
-            string directoryOutput = "";
-            ReportFormat format = ReportFormat.XML;
+            CommandLine.Parser.Default.ParseArguments<CommandOptions>(args)
+                .WithParsed<CommandOptions>(opts => CheckParsed(opts))
+                .WithParsed<CommandOptions>(opts => RunOptionsAndReturnExitCode(opts));
+        }
 
+        private static void CheckParsed(CommandOptions opts)
+        {
+            if (!File.Exists(opts.InputFileName))
+                throw new FileNotFoundException($"Specified file {opts.InputFileName} does not exists.");
+
+            if(!Directory.Exists(opts.OutputDirectory))
+                throw new DirectoryNotFoundException($"Specified Output Directory {opts.OutputDirectory} does not exists.");
+        }
+
+        private static void RunOptionsAndReturnExitCode(CommandOptions opts)
+        {
             ExaminationSet set = new ExaminationSet();
-            set.Deserialize(pathInput);
+            set.Deserialize(opts.InputFileName);
 
-            set.MakeReports(directoryOutput, format);
+            set.MakeReports(opts.OutputDirectory, opts.ReportFormat);
         }
     }
 }
